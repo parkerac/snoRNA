99b2b64c269e1793fc20bd4e368ae715b0237b91
Alyssa Parker <alyssaclaire31@gmail.com>
2026-07-01 15:36:18 +0100

    Initial commit

---
+++ review_dnm_vcf.py
@@ -0,0 +1,28 @@
+import cyvcf2
+
+vcf_path = "input.vcf"
+
+vcf = cyvcf2.VCF(vcf_path)
+
+print("samples:", vcf.samples)
+print("header lines:", len(vcf.raw_header.splitlines()))
+
+for variant in vcf:
+    chrom = variant.CHROM
+    pos = variant.POS
+    ref = variant.REF
+    alt = ",".join(variant.ALT)
+    qual = variant.QUAL
+    filter_pass = variant.FILTER
+    info = variant.INFO
+
+    print(f"{chrom}:{pos} {ref}>{alt} QUAL={qual} FILTER={filter_pass}")
+    print("  INFO keys:", list(info.keys()))
+
+    for sample in vcf.samples:
+        gt = variant.genotypes[vcf.samples.index(sample)]
+        # gt is [allele1, allele2, phased]
+        print(f"  {sample} GT={gt[0]}/{gt[1]} phased={gt[2]}")
+
+    # stop after the first variant if you only want a preview
+    break
